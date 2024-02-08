5df1373b4dece9af4fda4b33f25d3bf3ea00c61e
kevin <kevinbiantuadi@gmail.com>
2024-02-08 01:37:33 +0100

    ✅ Ajouter le composant d'en-tête, le menu de navigation et l'écran de survol dans le tableau de bord

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,11 +1,20 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import Header from "./header/Header";
+import Navmenu from "./navbar/Navmenu";
+import Hoverview from "./screens/hover_view/Hoverview";
 
 export default function Dashboard() {
   return (
-    <DashboardStyled>Dashboard</DashboardStyled>
-  )
+    <DashboardStyled>
+      <Header />
+      <main>
+        <Navmenu />
+        <Hoverview />
+      </main>
+    </DashboardStyled>
+  );
 }
 
 const DashboardStyled = styled.div`
   color: red;
-`
+`;
---
+++ src/pages/dashboard/screens/hover_view/Hoverview.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Hoverview() {
+  return (
+    <div>Hoverview</div>
+  )
+}
